73c9f623293a905a8bda6279b46b9568a8ec9b2a
schalkdaniel <d.schalk@mail.de>
2017-12-09 13:18:47 +0100

    Update some stuff

--- src/baselearner.cpp
+++ src/baselearner.cpp
@@ -1,76 +1,58 @@
-// // ========================================================================== \\
-// //                                 ___.                          __           \\
-// //        ____  ____   _____ ______\_ |__   ____   ____  _______/  |_         \\
-// //      _/ ___\/  _ \ /     \\____ \| __ \ /  _ \ /  _ \/  ___/\   __\        \\
-// //      \  \__(  <_> )  Y Y  \  |_> > \_\ (  <_> |  <_> )___ \  |  |          \\
-// //       \___  >____/|__|_|  /   __/|___  /\____/ \____/____  > |__|          \\
-// //           \/            \/|__|       \/                  \/                \\
-// //                                                                            \\
-// // ========================================================================== \\
-// //
-// // Compboost is free software: you can redistribute it and/or modify
-// // it under the terms of the GNU General Public License as published by
-// // the Free Software Foundation, either version 3 of the License, or
-// // (at your option) any later version.
-// // Compboost is distributed in the hope that it will be useful,
-// // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// // GNU General Public License for more details.
-// // You should have received a copy of the GNU General Public License
-// // along with Compboost. If not, see <http://www.gnu.org/licenses/>.
-// //
-// // This file contains:
-// // -------------------
-// //
-// //   "Baselearner" class
-// //
-// // Written by:
-// // -----------
-// //
-// //   Daniel Schalk
-// //   Institut für Statistik
-// //   Ludwig-Maximilians-Universität München
-// //   Ludwigstraße 33
-// //   D-80539 München
-// //
-// //   https://www.compstat.statistik.uni-muenchen.de
-// //
-// // =========================================================================== #
-// 
-// 
-// #include "baselearner.h"
-// #include <iostream>
-// 
-// namespace blearner {
-// 
-// // --------------------------------------------------------------------------- #
-// // Constructors:
-// // --------------------------------------------------------------------------- #
-// 
-// Baselearner::Baselearner (arma::vec response_ptr0, arma::mat data0,
-//   std::string var_name0, std::string blearner_name0, arma::vec parameter0)
-// {
-//   std::cout << "A new Baselearner object has ben created!" << std::endl;
-// 
-//   response_ptr = &response_ptr0;
-//   data = data0;
-//   var_name = var_name0;
-//   blearner_name = blearner_name0;
-//   parameter = parameter0;
-// }
-// 
-// // --------------------------------------------------------------------------- #
-// // Member functions:
-// // --------------------------------------------------------------------------- #
-// 
-// void Compboost::SetResponse (arma::vec response0)
-// {
-//   response = response0;
-// }
-// 
-// arma::vec Compboost::GetResponse ()
-// {
-//   return response;
-// }
-// 
-// } // namespace cboost
+// ========================================================================== //
+//                                 ___.                          __           //
+//        ____  ____   _____ ______\_ |__   ____   ____  _______/  |_         //
+//      _/ ___\/  _ \ /     \\____ \| __ \ /  _ \ /  _ \/  ___/\   __\        //
+//      \  \__(  <_> )  Y Y  \  |_> > \_\ (  <_> |  <_> )___ \  |  |          //
+//       \___  >____/|__|_|  /   __/|___  /\____/ \____/____  > |__|          //
+//           \/            \/|__|       \/                  \/                //
+//                                                                            //
+// ========================================================================== //
+//
+// Compboost is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// Compboost is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with Compboost. If not, see <http://www.gnu.org/licenses/>.
+//
+// This file contains:
+// -------------------
+//
+//   "Baselearner" class
+//
+// Written by:
+// -----------
+//
+//   Daniel Schalk
+//   Institut für Statistik
+//   Ludwig-Maximilians-Universität München
+//   Ludwigstraße 33
+//   D-80539 München
+
+//   https://www.compstat.statistik.uni-muenchen.de
+//
+// =========================================================================== #
+
+
+#include "baselearner.h"
+#include <iostream>
+
+namespace blearner {
+
+// --------------------------------------------------------------------------- #
+// Constructors:
+// --------------------------------------------------------------------------- #
+
+
+
+// --------------------------------------------------------------------------- #
+// Member functions:
+// --------------------------------------------------------------------------- #
+
+
+
+} // namespace blearner
--- src/baselearner.h
+++ src/baselearner.h
@@ -1,63 +1,98 @@
-// // ========================================================================== \\
-// //                                 ___.                          __           \\
-// //        ____  ____   _____ ______\_ |__   ____   ____  _______/  |_         \\
-// //      _/ ___\/  _ \ /     \\____ \| __ \ /  _ \ /  _ \/  ___/\   __\        \\
-// //      \  \__(  <_> )  Y Y  \  |_> > \_\ (  <_> |  <_> )___ \  |  |          \\
-// //       \___  >____/|__|_|  /   __/|___  /\____/ \____/____  > |__|          \\
-// //           \/            \/|__|       \/                  \/                \\
-// //                                                                            \\
-// // ========================================================================== \\
-// //
-// // Compboost is free software: you can redistribute it and/or modify
-// // it under the terms of the GNU General Public License as published by
-// // the Free Software Foundation, either version 3 of the License, or
-// // (at your option) any later version.
-// // Compboost is distributed in the hope that it will be useful,
-// // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// // GNU General Public License for more details.
-// // You should have received a copy of the GNU General Public License
-// // along with Compboost. If not, see <http://www.gnu.org/licenses/>.
-// //
-// // This file contains:
-// // -------------------
-// //
-// //   "Baselearner" class
-// //
-// // Written by:
-// // -----------
-// //
-// //   Daniel Schalk
-// //   Institut für Statistik
-// //   Ludwig-Maximilians-Universität München
-// //   Ludwigstraße 33
-// //   D-80539 München
-// //
-// //   https://www.compstat.statistik.uni-muenchen.de
-// //
-// // =========================================================================== #
+// ========================================================================== \\
+//                                 ___.                          __           \\
+//        ____  ____   _____ ______\_ |__   ____   ____  _______/  |_         \\
+//      _/ ___\/  _ \ /     \\____ \| __ \ /  _ \ /  _ \/  ___/\   __\        \\
+//      \  \__(  <_> )  Y Y  \  |_> > \_\ (  <_> |  <_> )___ \  |  |          \\
+//       \___  >____/|__|_|  /   __/|___  /\____/ \____/____  > |__|          \\
+//           \/            \/|__|       \/                  \/                \\
+//                                                                            \\
+// ========================================================================== \\
 //
-// #ifndef BASELEARNER_H_
-// #define BASELEARNER_H_
+// Compboost is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// Compboost is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with Compboost. If not, see <http://www.gnu.org/licenses/>.
 //
-// #include <RcppArmadillo.h>
-// #include <string>
+// This file contains:
+// -------------------
 //
-// namespace blearner {
+//   "Baselearner" class
 //
-// class Baselearner
-// {
-// private:
-//   arma::vec *response_ptr; // Pointer to the response
-//   arma::mat data; // Data for the baselearner e.g. one variable for linear bl
-//   std::string var_name; // Variable name, needed to know which variable are wrapped
-//   std::string blearner_name; // Name of the baselearner
-//   arma::vec parameter; // Vector of parameters. At the moment just numeric params
+// Written by:
+// -----------
 //
-// public:
-//   Baselearner(arma::vec, arma::mat, std::string, std::string, arma::vec);
-// };
+//   Daniel Schalk
+//   Institut für Statistik
+//   Ludwig-Maximilians-Universität München
+//   Ludwigstraße 33
+//   D-80539 München
 //
-// } // namespace cboost
+//   https://www.compstat.statistik.uni-muenchen.de
 //
-// #endif // BASELEARNER_H_
+// =========================================================================== #
+
+#ifndef BASELEARNER_H_
+#define BASELEARNER_H_
+
+#include <RcppArmadillo.h>
+#include <string>
+
+namespace blearner {
+
+// Parent class:
+// -----------------------
+
+// The parent class includes the minimal functionality every baselearner
+// must have.
+
+class Baselearner
+{
+  private:
+  
+    arma::mat data;
+    std::string blearner_identifier;
+  
+  public:
+  
+    Baselearner (std::string);
+    
+    void SetData (arma::mat);
+    arma::mat GetData ();
+    
+    // Functions which will be overloaded by the child functions. 
+    virtual void train () = 0;
+    virtual arma::mat predict () = 0;
+    virtual arma::mat predict (arma::mat newdata) = 0;
+    
+};
+
+// --------------------------------------------------------------------------- #
+// Child classes, acutal the real baselearner:
+// --------------------------------------------------------------------------- #
+
+// linear baselearner:
+// -----------------------
+
+class Linear: public Baselearner
+{
+  private:
+    
+    arma::vec parameter;
+    
+  public:
+    
+    void train ();
+    arma::mat predict ();
+    arma::mat predict (arma::mat newdata);
+    
+};
+
+} // namespace cboost
+
+#endif // BASELEARNER_H_
