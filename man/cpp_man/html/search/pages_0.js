5778d0a6315ffc17c025e35da07a25df4e767fc5
schalkdaniel <d-schalk@t-online.de>
2018-03-29 07:51:22 +0200

    update c++ manual

---
+++ man/cpp_man/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['framework_20for_20componentwise_20boosting',['Framework for COMPonentwise BOOSTing',['../index.html',1,'']]]
+];
--- man/cpp_man/html/search/searchdata.js
+++ man/cpp_man/html/search/searchdata.js
@@ -7,7 +7,8 @@ var indexSectionsWithContent =
   4: "_abcdfgilopqrst~",
   5: "abcdegiklmnoprstux",
   6: "bilpt",
-  7: "c"
+  7: "c",
+  8: "f"
 };
 
 var indexSectionNames =
@@ -19,7 +20,8 @@ var indexSectionNames =
   4: "functions",
   5: "variables",
   6: "typedefs",
-  7: "defines"
+  7: "defines",
+  8: "pages"
 };
 
 var indexSectionLabels =
@@ -31,6 +33,7 @@ var indexSectionLabels =
   4: "Functions",
   5: "Variables",
   6: "Typedefs",
-  7: "Macros"
+  7: "Macros",
+  8: "Pages"
 };
 
