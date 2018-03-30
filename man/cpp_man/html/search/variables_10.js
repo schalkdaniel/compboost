f57ba2d136f26ae08f78704a3b6b986c92502e07
schalkdaniel <d.schalk@mail.de>
2018-03-30 23:16:45 +0200

    update css for c++ doc

--- man/cpp_man/html/search/variables_10.js
+++ man/cpp_man/html/search/variables_10.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['use_5fcustom_5foffset',['use_custom_offset',['../classloss_1_1_loss.html#a5fabbbb104e6f430498630130ac8a131',1,'loss::Loss']]],
   ['used_5fbaselearner_5flist',['used_baselearner_list',['../classcboost_1_1_compboost.html#ac4c690473dc39e10e84ae9d9219b1fa1',1,'cboost::Compboost']]],
   ['used_5flogger',['used_logger',['../classcboost_1_1_compboost.html#a05590928bf741eecb135f32da339ceaa',1,'cboost::Compboost']]],
   ['used_5floss',['used_loss',['../classcboost_1_1_compboost.html#a9c776faf5e9b9e99b5241f2a650d5242',1,'cboost::Compboost::used_loss()'],['../classlogger_1_1_inbag_risk_logger.html#afee746bf4009661f930ea8294e7f72fc',1,'logger::InbagRiskLogger::used_loss()'],['../classlogger_1_1_oob_risk_logger.html#a40d02b9eec15e823bb2d71cf2112d6ed',1,'logger::OobRiskLogger::used_loss()']]],
