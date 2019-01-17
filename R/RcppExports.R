48ebb9335f517705898c83e7e8d05386650cb620
Maximilian Kaiser <32738992+illusive-git@users.noreply.github.com>
2019-01-17 17:23:09 +0100

    Update RcppExports.R
    Removing manual add of ResponseFDA from RcppExport

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -822,23 +822,6 @@ NULL
 #' @export ResponseBinaryClassif
 NULL
 
-#' Create response object for binary classification.
-#'
-#' \code{ResponseFDA} creates a response object that are used as target during the
-#' fitting process
-#'
-#' @format \code{\link{S4}} object.
-#' @name ResponseFDA
-#'
-#' @section Usage:
-#' \preformatted{
-#' ResponseFDA$new(target_name, response)
-#' ResponseFDA$new(target_name, response, weights)
-#' }
-#'
-#' @export ResponseFDA
-NULL
-
 #' Logger class to log the current iteration
 #'
 #' @format \code{\link{S4}} object.
