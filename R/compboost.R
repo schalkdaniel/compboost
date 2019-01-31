f67e4030044e82b73106662790c494e783adab0b
illusive-git <kaiservonsanfran@gmail.com>
2019-01-31 18:43:25 +0100

    added multi_dim

--- R/compboost.R
+++ R/compboost.R
@@ -340,7 +340,7 @@ Compboost = R6::R6Class("Compboost",
       if (! isRcppClass(target, "Response")) {
         if (! target %in% names(data)) {
           stop ("The target ", target, " is not present within the data")
-        }
+        } 
       }
       if (inherits(loss, "C++Class")) {
         stop ("Loss should be an initialized loss object by calling the constructor: ", deparse(substitute(loss)), "$new()")
