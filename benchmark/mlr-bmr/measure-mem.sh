199f619a665cc3e261c9a9dd06208bba85576e20
schalkdaniel <d-schalk@t-online.de>
2021-04-07 12:59:02 +0000

    edit memory measuring

--- benchmark/mlr-bmr/measure-mem.sh
+++ benchmark/mlr-bmr/measure-mem.sh
@@ -3,7 +3,7 @@
 filename=$1
 while true
 do
-  free -k | awk -v date="$(date +%Y-%m-%d-%H:%M)" 'FNR == 2 {print date ": " $2 " kb"}' >> $filename
+  free -k | awk -v date="$(date +%Y-%m-%d-%H:%M)" 'FNR == 2 {print date ": " $3 " kb"}' >> $filename
   sleep 10
 done
 
