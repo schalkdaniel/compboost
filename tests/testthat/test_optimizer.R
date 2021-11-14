2596faff9769d74d6304b05c1467fc35adb27c25
schalkdaniel <d-schalk@t-online.de>
2021-11-14 21:53:05 +0100

    add test for AGBM

--- tests/testthat/test_optimizer.R
+++ tests/testthat/test_optimizer.R
@@ -3,20 +3,20 @@ context("Optimizer of 'compboost'")
 test_that("Coordinate Descent with line search works", {
   n_train = 1000L
 
-  used_optimizer = OptimizerCoordinateDescent$new()
-  
+  expect_silent({ used_optimizer = OptimizerCoordinateDescent$new() })
+
   cboost = Compboost$new(data = mtcars, target = "mpg", optimizer = used_optimizer, loss = LossQuadratic$new(), learning_rate = 0.05)
-  
+
   cboost$addBaselearner("wt", "linear", BaselearnerPolynomial)
   cboost$addBaselearner("disp", "linear", BaselearnerPolynomial)
   cboost$addBaselearner("hp", "linear", BaselearnerPolynomial)
-  
+
   nuisance = capture.output(suppressWarnings({
     cboost$train(n_train)
   }))
-  
+
   used_optimizer_ls = OptimizerCoordinateDescentLineSearch$new()
-  
+
   nuisance = capture.output(suppressWarnings({
     cboost1 = Compboost$new(data = mtcars, target = "mpg", optimizer = used_optimizer_ls, loss = LossQuadratic$new(), learning_rate = 0.05)
   }))
@@ -24,7 +24,7 @@ test_that("Coordinate Descent with line search works", {
   cboost1$addBaselearner("wt", "linear", BaselearnerPolynomial)
   cboost1$addBaselearner("disp", "linear", BaselearnerPolynomial)
   cboost1$addBaselearner("hp", "linear", BaselearnerPolynomial)
-  
+
   nuisance = capture.output(suppressWarnings({
     cboost1$train(n_train)
   }))
@@ -33,4 +33,42 @@ test_that("Coordinate Descent with line search works", {
   expect_equal(cboost$getInbagRisk(), cboost1$getInbagRisk())
   expect_true(all(abs(used_optimizer_ls$getStepSize() - 1) < 1e10))
   expect_true(var(used_optimizer_ls$getStepSize()) < 1e-10)
+})
+
+test_that("AGBM optimizer works", {
+  n_train = 1000L
+
+  expect_silent({ used_optimizer = OptimizerAGBM$new(0.2) })
+
+  expect_silent({
+    cboost = Compboost$new(data = mtcars, target = "mpg", optimizer = used_optimizer, loss = LossQuadratic$new(),
+      learning_rate = 0.05)
+
+    cboost$addBaselearner("wt", "linear", BaselearnerPolynomial)
+    cboost$addBaselearner("disp", "linear", BaselearnerPolynomial)
+    cboost$addBaselearner("hp", "linear", BaselearnerPolynomial)
+  })
+  nuisance = capture.output(suppressWarnings({
+    cboost$train(n_train)
+  }))
+
+  expect_equal(length(used_optimizer$getSelectedMomentumBaselearner()), n_train)
+  expect_equal(cboost$predict(mtcars), cboost$predict())
+  expect_equal(used_optimizer$getStepSize(), 1)
+  expect_equal(cboost$response$getPrediction(), cboost$predict(mtcars))
+
+  expect_silent({ used_optimizer2 = OptimizerAGBM$new(0.2) })
+  expect_silent({
+    cboost2 = Compboost$new(data = mtcars, target = "mpg", optimizer = used_optimizer2, loss = LossQuadratic$new(),
+      learning_rate = 0.05)
+
+    cboost2$addBaselearner("wt", "linear", BaselearnerPolynomial)
+    cboost2$addBaselearner("disp", "linear", BaselearnerPolynomial)
+    cboost2$addBaselearner("hp", "linear", BaselearnerPolynomial)
+  })
+  nuisance = capture.output(suppressWarnings({
+    cboost2$train(1500)
+  }))
+  expect_output(cboost$train(1500))
+  expect_equal(cboost$predict(), cboost2$predict())
 })
