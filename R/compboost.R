09ed84bb29c9b32b75ce91bf5be4a7318133734d
schalkdaniel <d-schalk@t-online.de>
2023-02-04 10:12:47 +0100

    update compboost example

--- R/compboost.R
+++ R/compboost.R
@@ -1159,11 +1159,6 @@ Compboost = R6::R6Class("Compboost",
     #'
     #' @param file (`character(1)`)\cr
     #'   A data frame containing the data.
-    #' @examples
-    #' cboost = boostLinear(data = iris, target = "Sepal.Length", loss = LossQuadratic$new(),
-    #'   oob_fraction = 0.3)
-    #' cboost$model$saveJson("cboost.json")
-    #' cboost2 = cboost$new(file = "cboost.json")
     loadFromJson = function(file) {
       checkmate::assertFile(file, extension = c("json", "JSON", "Json"))
 
